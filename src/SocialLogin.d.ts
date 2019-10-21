da6e4d33db175e1ec4f95c1d2d40bbefe0bd8f2f
Adam Lane <adam@whutsfree.com>
2019-10-21 15:36:32 -0600

    Small updates to work with renamed package

--- src/SocialLogin.d.ts
+++ src/SocialLogin.d.ts
@@ -1,1 +1,8 @@
-export * from "./SocialLogin.android";
+import { IInitializationResult, ILoginResult, Social } from "./SocialLogin-common";
+export declare class SocialLogin extends Social {
+    init(result: IInitializationResult): IInitializationResult;
+    loginWithFacebook(callback: (result: Partial<ILoginResult>) => void): void;
+    loginWithGoogle(callback: (result: Partial<ILoginResult>) => void): void;
+    loginWithTwitter(callback: (result: Partial<ILoginResult>) => void): void;
+    logOut(callback: () => void): void;
+}
